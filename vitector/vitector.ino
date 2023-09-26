7c2e9453eb8b565797cdaf8be8a1a6536595ac32
one <imone075@gmail.com>
2023-09-26 11:44:28 +0900

    nothing

--- vitector/vitector.ino
+++ vitector/vitector.ino
@@ -1,14 +1,34 @@
+// ACS712, ZMPT101B sensor sampling >> get data and save
+// it will analyzed by python
+
 const int resVolPin = A0;
 const int indVolPin = A1;
-const int resIntPin = A2;
-const int indIntPin = A3;
+const int resCurPin = A2;
+const int indCurPin = A3;
 
-float acs = 0;
+float resVol, indVol, resCur, indCur = 0.0;
 
 void setup(){
     Serial.begin(9600);
 }
 
 void loop(){
-    AcsValueF = (2.5 - (avgAcs * (5.0 / 1024.0)) ) / 0.185;
+    checkCur(resCurPin);
+    checkCur(indCurPin);
+}
+
+float checkVol(int pin){
+    
+}
+
+float checkCur(int pin){
+    float acsValue, samples, avgAcs = 0.0;
+
+    for (int x; x < 150; x++){
+        acsValue = analogRead(pin);
+        samples += acsValue;
+        delayMicroseconds(3000);
+    }
+    avgAcs = (2.5 - ((samples / 150.0) * (5.0 / 1024.0))) / 0.185;
+    return avgAcs;
 }
