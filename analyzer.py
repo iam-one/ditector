3b5521aca5eff15765e71e7dd36eae84bc3370cc
one <imone075@gmail.com>
2023-10-30 16:06:02 +0900

    init

--- analyzer.py
+++ analyzer.py
@@ -1,19 +1,21 @@
 import argparse
-import math
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from scipy.optimize import curve_fit
 
 from config import DATA_PATH
 
 resVol, indVol, resCur, indCur, timeStamp = [], [], [], [], []
 
-timeStamp = [lambda x: x, range(0, 15)]
-resCur = [0, 1, 2, 3, 4, 3, 2, 1, 0, -1, -2, -3, -4, -3, -2]
+sampleCur = [-1, 0, 3, 6, 9, 12, -7]
+sampleTime = np.linspace(0, len(sampleCur) * 0.25, len(sampleCur))
 
-def curSin(x, a, b, c):
-    return a * math.sin(b * x) + c
+maxCur = []
+minCur = []
+
+def sins(x, A, w):
+    theta = w * x
+    return A * np.sin(theta)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -30,9 +32,15 @@ if __name__ == "__main__":
 
             #TODO: show plt graph for 4 datas by timestamp
             #TODO: get max/min of Current
-            #TODO: get sin-shape function for intensity of current
 
-        popt, pcov = curve_fit(curSin, timeStamp, resCur)
-        plt.plot(timeStamp, curSin(timeStamp, popt))
+        sample_x = np.linspace(0, 4 * np.pi, 1000)
 
-        plt.scatter(timeStamp, resVol, marker='.')
+        popt, pcov = curve_fit(sins, sampleTime, sampleCur)
+
+        print("Regression Sin Function: y = " + str(popt[0])[:8] + " sin(" + str(popt[1])[:8] + " * x)")
+        print("Max/Min Current(Real): " + " / ")
+        print("Max/Min Current(regression): " + str(popt[0])[:8] + " / " + str(-popt[0])[:9])
+        print("sin 함수의 peak가 실제 측정 시 최댓값과 차이가 크다면, 표본이 부족하여 회귀식을 생성하는 데 오차가 있을 확률이 높습니다.")
+
+        plt.plot(sample_x, sins(sample_x, *popt), color='red', linewidth=2)
+        plt.show()
